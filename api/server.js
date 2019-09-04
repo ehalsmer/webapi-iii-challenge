66582c9d3e5f1279ac15896748b8a448306a50fd
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-04 14:08:18 -0600

    Added logger middleware

--- api/server.js
+++ api/server.js
@@ -3,16 +3,20 @@ const express = require('express');
 const server = express();
 const usersRouter = require('./users/users-router');
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
 
-server.use('/users', usersRouter)
 
 //custom middleware
 
+// logger should be applied to all requests
 function logger(req, res, next) {
-
+  console.log(req.method, req.originalUrl, new Date())
+  next();
 };
+
+server.get('/', logger, (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`)
+});
+
+server.use('/users', logger, usersRouter)
 
 module.exports = server;
