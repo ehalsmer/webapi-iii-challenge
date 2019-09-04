2f788ec67bc1609926f6f411bce30e09b5fa8b64
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-04 16:39:43 -0600

    postRouter complete

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -24,6 +24,15 @@ function validatePostId(req, res, next) {
     } else { res.status(400).json({message: 'provide a post id'})}
 };
 
+function validatePost(req, res, next) {
+    if (Object.keys(req.body).length == 0) {
+      res.status(400).json({ message: "missing post data" });
+    } else if (!req.body.text) {
+      res.status(400).json({ message: "missing required text field" });
+    } 
+    next();
+  }
+
 router.get('/', (req, res) => {
     postDb.get()
     .then(response => {
@@ -34,16 +43,29 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+    res.status(200).json(req.post);
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, (req, res) => {
+    const id = req.params.id
+    postDb.remove(id)
+    .then(response => {
+        res.status(200).json(req.post)
+    })
+    .catch(error => {
+        res.status(500).json({message: "error removing post"})
+    })
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, validatePost, (req, res) => {
+    const userId = req.params.id
+    const updatedPost = {...req.post, ...req.body}
+    postDb.update(userId, updatedPost)
+    .then(response => {
+        res.status(201).json(updatedPost);
+    })
+    .catch(error => res.status(500).json({ error: "error updating post" }))
 });
 
 
