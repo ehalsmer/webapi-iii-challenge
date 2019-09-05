fc8976f283fcacc5b15665d03a783fe05a32829e
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-05 16:35:13 -0600

    Added dropdown for users on menu

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Menu, Dropdown } from "semantic-ui-react";
 
 const Nav = () => {
-    return (
-        <Menu>
-            <Menu.Item as={Link} to={'/'}>Home</Menu.Item>
-            <Menu.Item as={Link} to={'/allposts'}>All Posts</Menu.Item>
-            <Menu.Item as={Link} to={'/users'}>Users</Menu.Item>
-        </Menu>
-    )
-}
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ehalsmer-blog.herokuapp.com/users")
+      .then(response => {
+        console.log(response);
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <Menu>
+      <Menu.Item as={Link} to={"/"}>
+        Home
+      </Menu.Item>
+      <Menu.Item as={Link} to={"/allposts"}>
+        All Posts
+      </Menu.Item>
+      {/* <Menu.Item>Users</Menu.Item> */}
+      <Dropdown item text="Users">
+        <Dropdown.Menu>
+            {users.map((user)=><Menu.Item>{user.name}</Menu.Item>)}
+        </Dropdown.Menu>
+      </Dropdown>
+    </Menu>
+  );
+};
 
 export default Nav;
--- client/src/userList.js
+++ client/src/userList.js
@@ -1,26 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Accordion } from "semantic-ui-react";
+import UserAccord from './userAccord';
 
 const UserList = () => {
-    const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-    useEffect(()=>{
-        axios.get('https://ehalsmer-blog.herokuapp.com/users')
-        .then(response => {
-            console.log(response)
-            setUsers(response.data)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get("https://ehalsmer-blog.herokuapp.com/users")
+      .then(response => {
+        console.log(response);
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-    return(
-        <div>
-            <h1>User List</h1>
-            {users.map((user) => <div>{user.name}</div>)}
-        </div>
-    )
-}
+  return (
+    <>
+      <h1>User List</h1>
+      <Accordion>
+        {users.map(user => (
+          <UserAccord user={user}/> 
+        ))}
+      </Accordion>
+    </>
+  );
+};
 
 export default UserList;
